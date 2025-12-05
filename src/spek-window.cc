MikeWang000000/spek-X

Save PNG via commandline; Closes #1

src/spek-window.cc
@@ -51,8 +51,8 @@ private:
     SpekWindow *window;
 };
 
-SpekWindow::SpekWindow(int width, int height, const wxString& path) :
-    wxFrame(NULL, -1, wxEmptyString, wxDefaultPosition, wxSize(width, height)), path(path)
+SpekWindow::SpekWindow(int width, int height, const wxString& path, const wxString& pngpath) :
+    wxFrame(NULL, -1, wxEmptyString, wxDefaultPosition, wxSize(width, height)), path(path), pngpath(pngpath)
 {
     this->description = _("Spek - Acoustic Spectrum Analyser");
     SetTitle(this->description);
@@ -161,7 +161,7 @@ void SpekWindow::open(const wxString& path)
         wxString title = wxString::Format(_("Spek - %s"), full_name.c_str());
         SetTitle(title);
 
-        this->spectrogram->open(path);
+        this->spectrogram->open(path, this->pngpath);
     }
 }
 

src/spek-window.h
@@ -7,7 +7,7 @@ class SpekSpectrogram;
 class SpekWindow : public wxFrame
 {
 public:
-    SpekWindow(int width, int height, const wxString& path);
+    SpekWindow(int width, int height, const wxString& path, const wxString& pngpath);
     void open(const wxString& path);
 
 private:
@@ -23,6 +23,7 @@ private:
 
     SpekSpectrogram *spectrogram;
     wxString path;
+    wxString pngpath;
     wxString cur_dir;
     wxString description;
 

Lichtenshtein/spek

src/spek-window.cc
@@ -51,8 +51,8 @@ private:
     SpekWindow *window;
 };
 
-SpekWindow::SpekWindow(int DEF_WIDTH, int DEF_HEIGHT, const wxString& path) :
-    wxFrame(NULL, -1, wxEmptyString, wxDefaultPosition, wxSize(DEF_WIDTH, DEF_HEIGHT)), path(path)
+SpekWindow::SpekWindow(int DEF_WIDTH, int DEF_HEIGHT, const wxString& path, const wxString& pngpath) :
+    wxFrame(NULL, -1, wxEmptyString, wxDefaultPosition, wxSize(DEF_WIDTH, DEF_HEIGHT)), path(path), pngpath(pngpath)
 {
     this->description = _("Spek - Acoustic Spectrum Analyser");
     SetTitle(this->description);
@@ -168,7 +168,7 @@ void SpekWindow::open(const wxString& path)
         wxString title = wxString::Format(_("Spek - %s"), full_name.c_str());
         SetTitle(title);
 
-        this->spectrogram->open(path);
+        this->spectrogram->open(path, this->pngpath);
     }
 }
 

src/spek-window.h
@@ -7,7 +7,7 @@ class SpekSpectrogram;
 class SpekWindow : public wxFrame
 {
 public:
-    SpekWindow(int DEF_WIDTH, int DEF_HEIGHT, const wxString& path);
+    SpekWindow(int DEF_WIDTH, int DEF_HEIGHT, const wxString& path, const wxString& pngpath);
     void open(const wxString& path);
 
 private:
@@ -23,6 +23,7 @@ private:
 
     SpekSpectrogram *spectrogram;
     wxString path;
+    wxString pngpath;
     wxString cur_dir;
     wxString description;
 